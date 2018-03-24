e42df6d598b65649dda5d469ec9ccb4ebc915879
peter1403 <peter1403@hotmail.co.uk>
2018-03-24 00:50:20 +0000

    Basic login implemented

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,36 +1,3 @@
 //# Place all the behaviors and hooks related to the matching controller here.
 //# All this logic will automatically be available in application.js.
 //# You can use CoffeeScript in this file: http://coffeescript.org/
-$(document).ready(function() {
-  var delay=2700, setTimeoutConst;
-  $('#navToggle').hover (function() {
-    div = $(this);
-    div.addClass("HoverBorder");
-     setTimeoutConst = setTimeout(function(){
-       $('#navMenu').toggle();
-       div.toggleClass("navOpen")
-     }, delay);
-   }, function(){
-     div.removeClass("HoverBorder");
-     clearTimeout(setTimeoutConst );
-  });
-  $('#navToggle').click (function(){
-    div = $(this);
-    $('#navMenu').toggle();
-    div.toggleClass("navOpen")
-    clearTimeout(setTimeoutConst );
-  })
-
-  $('.modalClick').hover (function() {
-    div = $(this);
-    href = div.parent().attr("href");
-    div.addClass("HoverBorder");
-     setTimeoutConst = setTimeout(function(){
-       window.location.href = href;
-     }, delay);
-   }, function(){
-     div.removeClass("HoverBorder");
-     clearTimeout(setTimeoutConst );
-  });
-
-});
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   root 'users#home'
   get '/index', to: 'users#index'
   get '/signup', to: 'users#new'
   post '/signup',  to: 'users#create'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
+
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
