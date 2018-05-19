08b02b7ff8dad1ae3cf4562ca708c0219dcccd02
peter1403 <peter1403@hotmail.co.uk>
2018-05-19 21:06:55 +0100

    Try 3

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,12 +1,13 @@
 if Rails.env.production?
   CarrierWave.configure do |config|
-    config.fog_credentials = {
-      # Configuration for Amazon S3
-      :provider              => 'AWS',
-      :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
-      :aws_secret_access_key => ENV['S3_SECRET_KEY'],
-      :region                => ENV['S3_REGION']
-    }
-    config.fog_directory     =  ENV['S3_BUCKET']
-  end
+  config.fog_provider = 'fog/aws'                        # required
+  config.fog_credentials = {
+    provider:              'AWS',                        # required
+    aws_access_key_id:     ENV['S3_ACCESS_KEY'],                        # required
+    aws_secret_access_key: ENV['S3_SECRET_KEY'],                        # required
+    region:                ENV['S3_REGION']                  # optional, defaults to 'us-east-1'
+  }
+  config.fog_directory  = ENV['S3_BUCKET']                                   # required
+  config.fog_public     = false                                                 # optional, defaults to true
+  config.fog_attributes = { cache_control: "public, max-age=#{365.days.to_i}" } # optional, defaults to {}
 end
